06d4bf53c54e2527f38c71cd48896bb501fa214c
William Bae <wbae912@gmail.com>
2019-10-10 10:06:04 -0700

    ready for deploy

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -9,7 +9,7 @@ describe('Bookmarks Endpoint', () => {
   before('setup connection to DB before all tests', () => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set('db',db);
   });
